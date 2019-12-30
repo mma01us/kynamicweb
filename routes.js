b320b1a2981db8773d8d6f7b042238b7975564e4
Michael Ma <mma01us@gmail.com>
2019-12-30 14:42:59 -0500

    Light home page completed.

--- routes.js
+++ routes.js
@@ -4,9 +4,6 @@ const bodyParser = require("body-parser");
 var path = require("path");
 
 module.exports = function(app) {
-    app.get("", function(req, res){ // Any page requested
-        res.redirect("/"); // Force all to redirect to index page
-    });
     app.get("/", function(req, res){
         res.render("../src/views/index.ejs");
     });
