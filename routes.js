ae1b522b7e3279bd3ea2d6da6b9c90c5dcbd3d10
Michael Ma <mma01us@gmail.com>
2019-12-31 21:55:53 -0500

    Redirect all pages to root/index

--- routes.js
+++ routes.js
@@ -7,4 +7,7 @@ module.exports = function(app) {
     app.get("/", function(req, res){
         res.render("../src/views/index.ejs");
     });
+    app.use(function(req, res, next){ // Redirect all to home
+        res.redirect("/");
+    });
 };
